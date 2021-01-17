8410ce3d6fb0b86e5fab0e406d7f06735566aec8
Abby <sk573129@gmail.com>
2021-01-18 00:48:07 +0800

    chore: 調整tsconfig裡的paths設定

--- src/app/layout/content/random-match/random-match.module.ts
+++ src/app/layout/content/random-match/random-match.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../../../shared/shared.module';
-
+import { SharedModule } from '@shared/shared.module';
 import { RandomMatchComponent } from './random-match.component';
 
 @NgModule({
