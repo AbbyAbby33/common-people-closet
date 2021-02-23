5db9776aa175ea0735a331a07095b366ab628d0a
Abby <sk573129@gmail.com>
2021-02-23 22:43:01 +0800

    feat: (詳細故事) 加入分頁器樣板

--- src/app/layout/all-story/all-story.component.ts
+++ src/app/layout/all-story/all-story.component.ts
@@ -14,9 +14,49 @@ export class AllStoryComponent implements OnInit {
     {},
   ];
 
+  pageNumList = [
+    {pageNum: 1},
+    {pageNum: 2},
+    {pageNum: 3},
+    {pageNum: 4},
+    {pageNum: 5},
+  ];
+
+  // pageNumList = [{ pageNum: 1 }, { pageNum: 2 }, { pageNum: 3 }];
+
+  // pageNumList = [
+  //   { pageNum: 4 },
+  //   { pageNum: 5 },
+  //   { pageNum: 6 },
+  //   { pageNum: 7 },
+  //   { pageNum: 8 },
+  // ];
+
+  page: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // 1.算要顯示的pageNumList：(1)以被點的為中心 (2)可能不滿五個
+
+
+
+  // 注意：如果change每頁的筆數，最好回到第一頁會比較安全，因為該也可能不存在，比如說總筆數29，每頁筆數10，在第三頁，之後調整每頁筆數為50，則只會有一頁
+
+
+
+  // 2.拋出事件：哪一個pageNum被點擊
+  onPageClick(event: MouseEvent, pageNum: any) {
+    event.stopPropagation();
+    if (pageNum === 'pre' || pageNum === 'next') {
+      console.log(`${pageNum}`);
+    } else {
+      console.log(`點擊第${pageNum}頁`);
+    }
+  }
+
+
 
 }
