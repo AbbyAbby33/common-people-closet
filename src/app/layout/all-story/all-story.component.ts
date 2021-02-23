108be662b6d879c18af252c158feb8d3e2b5dbbb
Abby <sk573129@gmail.com>
2021-02-24 00:09:47 +0800

    feat: (詳細故事) 1.衣服假資料從service取 2.分頁邏輯(未完成)

--- src/app/layout/all-story/all-story.component.ts
+++ src/app/layout/all-story/all-story.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ClothInfoApiService } from '@core/service-api/cloth-info-api.service';
 
 @Component({
   selector: 'cpc-all-story',
   templateUrl: './all-story.component.html',
   styleUrls: ['./all-story.component.scss']
 })
-export class AllStoryComponent implements OnInit {
+export class AllStoryComponent implements OnInit, AfterViewInit {
 
-  clothList = [
-    {},
-    {},
-    {},
-    {},
-  ];
+  dataSource = new MatTableDataSource<any>();
+  length = 0;
+
+  pageIndex = 0;
+
+  clothListThisPage: Array<any>;
 
   pageNumList = [
     {pageNum: 1},
@@ -32,12 +34,46 @@ export class AllStoryComponent implements OnInit {
   //   { pageNum: 8 },
   // ];
 
-  page: number = 1;
 
-  constructor() { }
+  constructor(
+    private clothInfoApiService: ClothInfoApiService,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+    /** setTimeout：避免ExpressionChangedAfterItHasBeenCheckedError，真實串API的時候就不需要setTimeout */
+    setTimeout(() => this.getAllClothInfo(), 0);
+  }
+
+  /** 取得所有衣物資訊 */
+  getAllClothInfo() {
+    // this.dataSource.paginator = this.paginator;
+    this.dataSource.data = this.clothInfoApiService.getClothInfo();
+    this.length = this.clothInfoApiService.getClothInfo().length;
+    console.log('this.dataSource', this.dataSource);
+    this.getClothListThisPage(this.pageIndex);
+  }
+
+  /** 取得當頁應顯示衣物資訊 */
+  getClothListThisPage(pageIndex) {
+    const pageSize = 12;
+    this.clothListThisPage = this.dataSource.data.slice(pageIndex * 12, (pageIndex + 1) * 12);
+
+    // TODO: 記得處理最後一頁剩下一點點的狀況
+
+  }
+
+
+
+
+
+
+
+
+
+
 
   // 1.算要顯示的pageNumList：(1)以被點的為中心 (2)可能不滿五個
 
