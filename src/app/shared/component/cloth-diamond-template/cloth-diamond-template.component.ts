bf7e5b11d4d4676acc693fed4ca73da5cad86002
Abby <sk573129@gmail.com>
2021-02-24 00:18:07 +0800

    chore: 衣物鑽石樣板共用元件@Input()帶入資料

--- src/app/shared/component/cloth-diamond-template/cloth-diamond-template.component.ts
+++ src/app/shared/component/cloth-diamond-template/cloth-diamond-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cpc-cloth-diamond-template',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClothDiamondTemplateComponent implements OnInit {
 
+  @Input() clothItemInfo;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('this.clothItemInfo', this.clothItemInfo);
   }
 
 }
