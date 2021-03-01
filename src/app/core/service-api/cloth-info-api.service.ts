bf70a88befa9b3599dccde357db5c16e417ac390
Abby <sk573129@gmail.com>
2021-03-01 22:04:42 +0800

    feat: (詳細故事) 1.完成分頁器總頁數及換頁邏輯(沒有處理只要五個) 2.調整假資料

--- src/app/core/service-api/cloth-info-api.service.ts
+++ src/app/core/service-api/cloth-info-api.service.ts
@@ -8,23 +8,24 @@ export class ClothInfoApiService {
   /** 衣物資料 */
   clothInfo = [
     // type: 1:上身----(secondType: 1:長袖上衣 2:短袖上衣 3:襯衫),
+    // type: 2:下身----(secondType: 4:長褲 5:短褲 6:裙子),
+    // type: 3:外套----(secondType: null),
+    // type: 4:配件----(secondType: null),
+
     { id: 'aa-0000-0002', name: '毛絨感維尼蜂蜜上衣', material: '棉60% + 聚酯纖維40%', preference: 4, story: '', type: 1, secondType: 3, loginDate: '2020-10-01', mainImg: 'aaaa-0000-0033.jpg' },        // CACO
     { id: 'aa-0000-0001', name: '個性粗黑線棕色上衣', material: '棉60% + 聚酯纖維40%', preference: 3, story: '', type: 1, secondType: 1, loginDate: '2020-10-01', mainImg: 'aaaa-0000-0001.jpg' },        // CACO
     { id: 'aa-0000-0003', name: '單口袋細條紋上衣', material: '棉100%', preference: 1, story: '', type: 1, secondType: 3, loginDate: '2020-10-01', mainImg: 'aaaa-0000-0007.jpg' },                       // CACO
     { id: 'aa-0000-0004', name: '厚布料寬鬆帽T', material: '棉90% + 聚酯纖維10%', preference: 5, story: '', type: 1, secondType: 1, loginDate: '2021-01-10', mainImg: 'aaaa-0000-0008.jpg' },            // AJPeace
-    { id: 'aa-0000-0005', name: '溫暖刷毛大學帽T', material: '棉90% + 聚酯纖維10%', preference: 4, story: '', type: 1, secondType: 1, loginDate: '2016-08-01', mainImg: 'aaaa-0000-0009.jpg' },          // 伊利諾大學
-
+    { id: 'aa-0000-0023', name: '英文字圖案連帽休閒外套', material: '棉100%', preference: 2, story: '', type: 3, secondType: 4, loginDate: '2016-11-22', mainImg: 'aaaa-0000-0026.jpg' },
     { id: 'aa-0000-0007', name: '百搭不用想黑色素T', material: '棉100%', preference: 5, story: '', type: 1, secondType: 2, loginDate: '2018-05-08', mainImg: 'aaaa-0000-0010.jpg' },
-
     { id: 'aa-0000-0008', name: '前短後長大鈕釦襯衫', material: '棉20% + 聚酯纖維80%', preference: 5, story: '', type: 1, secondType: 3, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0011.jpg' },        // PAZZO
-    { id: 'aa-0000-0009', name: '流行染色感襯衫', material: '棉10% + 聚酯纖維90%', preference: 2, story: '', type: 1, secondType: 3, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0012.jpg' },
+    { id: 'aa-0000-0022', name: '白色牛仔外套', material: '棉100%', preference: 3, story: '', type: 3, secondType: 4, loginDate: '2016-04-22', mainImg: 'aaaa-0000-0025.jpg' },
     { id: 'aa-0000-0010', name: '水彩圓點造型襯衫', material: '棉10% + 聚酯纖維90%', preference: 1, story: '', type: 1, secondType: 3, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0013.jpg' },          // Queen Shop
     { id: 'aa-0000-0011', name: '小格子配色排釦長袖襯衫', material: '棉30% + 聚酯纖維70%', preference: 5, story: '', type: 1, secondType: 3, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0014.jpg' },     // Queen Shop
     { id: 'aa-0000-0012', name: '素面襯衫', material: '棉100%', preference: 5, story: '', type: 1, secondType: 3, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0015.jpg' },                              // Queen Shop
     { id: 'aa-0000-0013', name: '杏色滿版花花襯衫', material: '棉30% + 聚酯纖維70%', preference: 1, story: '', type: 1, secondType: 3, loginDate: '2018-05-08', mainImg: 'aaaa-0000-0016.jpg' },
 
 
-    // type: 2:下身----(secondType: 4:長褲 5:短褲 6:裙子),
     { id: 'aa-0000-0014', name: '優雅格紋長裙', material: '聚酯纖維 10% 彈性纖維 90%', preference: 4, story: '', type: 2, secondType: 6, loginDate: '2021-01-10', mainImg: 'aaaa-0000-0017.jpg' },              // AJPeace
     { id: 'aa-0000-0015', name: '時尚壓線西裝長褲', material: '聚酯纖維 90% 彈性纖維 10%', preference: 3, story: '', type: 2, secondType: 4, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0018.jpg' },     // PAZZO
     { id: 'aa-0000-0016', name: '厚布料抽繩棉褲', material: '棉100%', preference: 3, story: '', type: 2, secondType: 4, loginDate: '2020-10-01', mainImg: 'aaaa-0000-0019.jpg' },                        // CACO
@@ -34,14 +35,12 @@ export class ClothInfoApiService {
     { id: 'aa-0000-0020', name: '素色壓線直筒褲', material: '棉80% + 聚酯纖維20%', preference: 5, story: '', type: 2, secondType: 4, loginDate: '2021-01-22', mainImg: 'aaaa-0000-0023.jpg' },                        // AJPeace
 
 
-    // type: 3:外套----(secondType: null),
     { id: 'aa-0000-0021', name: '率性防風飛行外套', material: '聚酯纖維 100%', preference: 5, story: '', type: 3, secondType: 4, loginDate: '2018-07-30', mainImg: 'aaaa-0000-0024.jpg' },
-    { id: 'aa-0000-0022', name: '白色牛仔外套', material: '棉100%', preference: 3, story: '', type: 3, secondType: 4, loginDate: '2016-04-22', mainImg: 'aaaa-0000-0025.jpg' },
-    { id: 'aa-0000-0023', name: '英文字圖案連帽休閒外套', material: '棉100%', preference: 2, story: '', type: 3, secondType: 4, loginDate: '2016-11-22', mainImg: 'aaaa-0000-0026.jpg' },
+    { id: 'aa-0000-0005', name: '溫暖刷毛大學帽T', material: '棉90% + 聚酯纖維10%', preference: 4, story: '', type: 1, secondType: 1, loginDate: '2016-08-01', mainImg: 'aaaa-0000-0009.jpg' },          // 伊利諾大學
     { id: 'aa-0000-0024', name: '鋪棉毛毛連帽外套', material: '聚酯纖維100%', preference: 2, story: '', type: 3, secondType: 4, loginDate: '2020-10-13', mainImg: 'aaaa-0000-0027.jpg' },
+    { id: 'aa-0000-0009', name: '流行染色感襯衫', material: '棉10% + 聚酯纖維90%', preference: 2, story: '', type: 1, secondType: 3, loginDate: '2021-01-28', mainImg: 'aaaa-0000-0012.jpg' },
 
 
-    // type: 4:配件----(secondType: null),
     { id: 'aa-0000-0025', name: '英文小字鴨舌帽', material: '棉100%', preference: 1, story: '', type: 4, secondType: 4, loginDate: '2017-05-13', mainImg: 'aaaa-0000-0028.jpg' },
     { id: 'aa-0000-0026', name: '背心', material: '聚酯纖維 70% 尼龍 30%', preference: 5, story: '', type: 4, secondType: 4, loginDate: '2019-11-30', mainImg: 'aaaa-0000-0029.jpg' },
     { id: 'aa-0000-0027', name: '黑色側背小包', material: '合成皮', preference: 5, story: '', type: 4, secondType: 4, loginDate: '2020-4-26', mainImg: 'aaaa-0000-0030.jpg' },
--- src/app/layout/all-story/all-story.component.ts
+++ src/app/layout/all-story/all-story.component.ts
@@ -11,29 +11,18 @@ export class AllStoryComponent implements OnInit, AfterViewInit {
 
   dataSource = new MatTableDataSource<any>();
   length = 0;
-
   pageIndex = 0;
-
-  clothListThisPage: Array<any>;
-
-  pageNumList = [
-    {pageNum: 1},
-    {pageNum: 2},
-    {pageNum: 3},
-    {pageNum: 4},
-    {pageNum: 5},
-  ];
-
-  // pageNumList = [{ pageNum: 1 }, { pageNum: 2 }, { pageNum: 3 }];
-
+  pageSize = 12;
+  pageNumList = [];
   // pageNumList = [
-  //   { pageNum: 4 },
-  //   { pageNum: 5 },
-  //   { pageNum: 6 },
-  //   { pageNum: 7 },
-  //   { pageNum: 8 },
+  //   {pageNum: 1},
+  //   {pageNum: 2},
+  //   {pageNum: 3},
+  //   {pageNum: 4},
+  //   {pageNum: 5},
   // ];
 
+  clothListThisPage: Array<any>;
 
   constructor(
     private clothInfoApiService: ClothInfoApiService,
@@ -54,46 +43,55 @@ export class AllStoryComponent implements OnInit, AfterViewInit {
     this.length = this.clothInfoApiService.getAllClothInfo().length;
     console.log('this.dataSource', this.dataSource);
     this.getClothListThisPage(this.pageIndex);
+    this.countPageNumList(this.length);
   }
 
   /** 取得當頁應顯示衣物資訊 */
   getClothListThisPage(pageIndex) {
-    const pageSize = 12;
-    // const pageSize = 32;
-    this.clothListThisPage = this.dataSource.data.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize);
-
-    // TODO: 記得處理最後一頁剩下一點點的狀況
-
+    this.pageIndex = pageIndex;
+    this.clothListThisPage = [];
+    // slice第二個參數是取到這個參數之前，故不需要另外處理最後一頁剩下一點點的狀況，不會有問題
+    this.clothListThisPage = this.dataSource.data.slice(pageIndex * this.pageSize, (pageIndex + 1) * this.pageSize);
   }
 
-
-
-
-
-
-
-
-
-
-
   // 1.算要顯示的pageNumList：(1)以被點的為中心 (2)可能不滿五個
+  countPageNumList(length: number) {
+    const fullPageNum = (length - length % this.pageSize ) / this.pageSize; // 滿一整頁的頁數
+    const pageNum = length % this.pageSize ? fullPageNum + 1 : fullPageNum;
+    // console.log('pageNum', pageNum);
 
-
-
-  // 注意：如果change每頁的筆數，最好回到第一頁會比較安全，因為該也可能不存在，比如說總筆數29，每頁筆數10，在第三頁，之後調整每頁筆數為50，則只會有一頁
-
-
+    for (let i = 0; i < pageNum; i++) {
+      this.pageNumList.push({pageNum: i + 1});
+    }
+    // console.log('this.pageNumList', this.pageNumList);
+  }
 
   // 2.拋出事件：哪一個pageNum被點擊
+  // 但我目前沒有開發調整pageSize的 => 注意：如果change每頁的筆數，最好回到第一頁會比較安全，因為該也可能不存在，比如說總筆數29，每頁筆數10，在第三頁，之後調整每頁筆數為50，則只會有一頁
   onPageClick(event: MouseEvent, pageNum: any) {
     event.stopPropagation();
     if (pageNum === 'pre' || pageNum === 'next') {
+      // 2-1.前一頁或後一頁
       console.log(`${pageNum}`);
+      console.log(this.pageIndex);
+      if (pageNum === 'pre') {
+        // 前
+        if (this.pageIndex !== 0) {
+          this.getClothListThisPage(this.pageIndex - 1);
+        }
+
+      } else {
+        // 後
+        if (this.pageIndex !== this.pageNumList.length - 1) {
+          this.getClothListThisPage(this.pageIndex + 1);
+        }
+      }
+
     } else {
+      // 2-2.頁數被點
       console.log(`點擊第${pageNum}頁`);
+      this.getClothListThisPage(pageNum - 1);
     }
   }
-
-
 
 }
