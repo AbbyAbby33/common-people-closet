cee90100efd318660f79c38c1d1d596ca5343736
Abby <sk573129@gmail.com>
2021-03-03 22:10:38 +0800

    feat: (首頁) loading page樣板及邏輯

--- src/app/shared/component/loading-page/loading-page.module.ts
+++ src/app/shared/component/loading-page/loading-page.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingPageComponent } from './loading-page.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -8,9 +11,15 @@ import { LoadingPageComponent } from './loading-page.component';
   ],
   imports: [
     CommonModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatIconModule,
   ],
   exports: [
     LoadingPageComponent,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatIconModule,
   ]
 })
 export class LoadingPageModule { }
